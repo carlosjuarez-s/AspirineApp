117fc648006a475bfdf9e064f2a613ece3033f7c
Carlos Juarez <carloe2000@gmail.com>
2022-03-21 23:51:23 -0300

    Init Folders

---
+++ models/personModel.js
@@ -0,0 +1,17 @@
+const mongoose = required('mongoose');
+
+const { Schema } = mongoose;
+
+const personModel = new Schema(
+    {
+        firstName: {type:String},
+		lastName: {type:String},
+		userName: {type:String},
+		password: {type:String},
+		email: {type:String},
+		address: {type:String},
+		phone: {type:Number}
+    }
+)
+
+module.exports = mongoose.model("Persons", personModel);
